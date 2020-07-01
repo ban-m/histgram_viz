1f4760c7e8ee30f077c3b66898d7b74550308b83
ban-m <ban-m@g.ecc.u-tokyo.ac.jp>
2020-07-01 12:43:06 +0900

    debug

--- src/lib.rs
+++ src/lib.rs
@@ -17,21 +17,22 @@ impl Histgram {
         // Determine the draw range.
         let min = *self.raw_count.iter().min().unwrap();
         let max = *self.raw_count.iter().max().unwrap();
-        let tick = (max - min) / row;
+        let tick = ((max - min) / row).max(1);
         let counts: Vec<_> = (0..row)
             .map(|i| {
                 self.raw_count
                     .iter()
                     .filter(|&&x| match i {
-                        0 => x <= min + tick,
-                        x if x == row => min + tick * row < x,
-                        _ => min + tick * i < x && x <= min + tick * (i + 1),
+                        0 => x < min + tick,
+                        x if x == row => min + tick * row <= x,
+                        _ => min + tick * i <= x && x < min + tick * (i + 1),
                     })
                     .count()
             })
             .collect();
         // Determine the header digit.
         let digit = format!("{}", max).chars().count();
+        let digit = digit.max(format!("{}", row).chars().count());
         // Determine column tick.
         let c_tick = counts.iter().max().unwrap() / column;
         use std::iter;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    let count: Vec<_> = (0..1000).map(|_| 1).collect();
+    let hist = histgram_viz::Histgram::new(&count);
+    eprintln!("Hist of Degree\n{}", hist.format(20, 40));
 }
